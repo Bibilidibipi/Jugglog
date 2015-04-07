327b80ecdde472fdda6b70f4e1445583f35acc90
Bibilidibipi <judy0wacker@gmail.com>
2015-04-07 13:20:10 -0700

    Finish user Log In and Sign Up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
-      render: new
+      render :new
     end
   end
 
